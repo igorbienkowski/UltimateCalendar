1fba87cef12463bafcde2c19ae65936b4f0bb9b3
Igor B <igorbienkowski@gmail.com>
2018-12-11 10:51:31 +0000

    Added generic DBQuery class which will be used to implement Template Design Pattern.

---
+++ UltimateCalendar/Models/DBQuery.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateCalendar.Models
+{
+    public abstract class DBQuery<AnyType>
+    {
+        protected MySqlConnection connection = null;
+        protected MySqlCommand command = null;
+
+        private void Open()
+        {
+            connection = new MySqlConnection(ConfigurationManager.ConnectionStrings["GCPMySqlDB"].ConnectionString);
+            connection.Open();
+            command = new MySqlCommand();
+            command.Connection = connection;
+        }
+
+        private void Execute(AnyType obj)
+        {
+            Open();
+
+            ExecuteCommand(obj);
+
+            Close();
+        }
+
+        private void Close()
+        {
+            connection.Dispose();
+        }
+
+        public abstract void ExecuteCommand(AnyType obj);
+    }
+}
